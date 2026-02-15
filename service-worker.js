c3b602e188b08ab722fc4d427c9d4722352f623e
antonino30 <antonino.curia30@gmail.com>
2026-02-15 10:55:02 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,20 @@
 const CACHE_NAME = "appstudio-v1";
-
 const ASSETS = [
   "./",
   "./index.html",
   "./style.css",
   "./app.js",
   "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
   "./math.json",
   "./geo.json",
-  "./music.json",
-  // aggiungi qui eventuali immagini fisse:
-  "./img/circuito_semplice.png"
-  // (le immagini di arte e gli audio possono essere tanti:
-  // vedi nota sotto per gestirli bene)
+  "./data.json",
+  "./music.json"
 ];
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
 
@@ -32,17 +28,5 @@ self.addEventListener("activate", (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cached) => {
-      if (cached) return cached;
-      return fetch(event.request)
-        .then((resp) => {
-          // cachea anche quello che scarica, così funziona offline dopo il primo giro
-          const copy = resp.clone();
-          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
-          return resp;
-        })
-        .catch(() => cached); // se offline e non in cache, torna quello che c’è
-    })
-  );
+  event.respondWith(caches.match(event.request).then((c) => c || fetch(event.request)));
 });
